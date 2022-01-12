42e236e9ef6f08ff86483fbc1e0a5e5ffe3730a4
sudharshankr-careator <sudharshan.kr@careator.com>
2022-01-12 16:13:15 +0530

    1 file modified

--- Hello.tsx
+++ Hello.tsx
@@ -6,14 +6,19 @@ export default function Hello({ name }) {
     b: 2,
     c: 3,
   };
+  var fruits="Banana Orange Apple Mango";
+ console.log(fruits.split(' ').reverse())
+ let a=["guru","reva", "kaju"];
+ 
   function sum(obj){
     return Object.keys(obj).reduce((sum, key)=>sum+ (obj[key]||0),0)
   }
 // let v = Object.values(obj).reduce((a, b)=> a+b)
-//   console.log(sum(obj))
+   console.log(sum(obj))
   return (
     <div>
       <h1>Hello {name}!</h1>
+      {a.map((val: any)=>val.length)}
     </div>
   );
 }
