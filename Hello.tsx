fa68d2b25499a0b56cf3b65d9b163a747e4b269f
sudharshankr-careator <sudharshan.kr@careator.com>
2022-01-12 16:01:16 +0530

    1 file modified

--- Hello.tsx
+++ Hello.tsx
@@ -1,3 +1,19 @@
 import React from 'react';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+export default function Hello({ name }) {
+  var obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+  function sum(obj){
+    return Object.keys(obj).reduce((sum, key)=>sum+ (obj[key]||0),0)
+  }
+// let v = Object.values(obj).reduce((a, b)=> a+b)
+//   console.log(sum(obj))
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+    </div>
+  );
+}
